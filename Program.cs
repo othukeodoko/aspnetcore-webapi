7733d92c1cd842c3d57a479d6f09271bdc43eb4e
othukeodoko <hercules.othuke@gmail.com>
2023-05-10 16:26:12 +0100

    Final change and push for the day

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using SimpleProject.Data.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using SimpleProject.Data.Services;
-using SimpleProject.Helper;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,9 +20,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-var scope = app.Services.CreateScope();
-await DataHelper.ManageDataAsync(scope.ServiceProvider);
-// Configure the HTTP request pipeline.
+
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
