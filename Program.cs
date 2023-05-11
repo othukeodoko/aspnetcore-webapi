deab4153085fc718329bddf0334af53675b0a557
othukeodoko <hercules.othuke@gmail.com>
2023-05-11 07:54:46 +0100

    Kestrel Stage

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ using SimpleProject.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if (args.Length > 0) // NOTE: arguments are path to cert.pfx file and its optional password
+{
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(443, listenOptions =>
+        {
+            listenOptions.UseHttps(args[0], args.Length > 1 ? args[1] : "");
+        });
+        serverOptions.ListenAnyIP(80); // NOTE: optionally listen on port 80, too
+    });
+}
+
 //DbContext configuration
 builder.Services.AddDbContext<AppDBContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 // Add services to the container.
